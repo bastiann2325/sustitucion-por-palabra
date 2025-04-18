7b4522399969e01fb6c2122a4708a465bf1bdac5
Juan Sebastian Mejia Zuniga <juansebas7844@gmail.com>
2025-04-18 15:05:37 -0500

    feat: Empezar a desarrollar lógica del algoritmo en js

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Palabra para la sustitución
+const WORD = "electroencefalografista"
+
+// Diccionario para asignar 
+for (let letter = 0; letter < WORD.length; index++) {
+  console.log(WORD[letter]);
+}
