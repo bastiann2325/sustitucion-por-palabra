25b463af4310097d0524dc403957d4a03f02043e
Juan Sebastian Mejia Zuniga <juansebas7844@gmail.com>
2025-04-18 16:27:02 -0500

    feat: Avanzar lógica funión para encriptar texto

--- js/app.js
+++ js/app.js
@@ -1,7 +1,34 @@
-// Palabra para la sustitución
-const WORD = "electroencefalografista"
+// Declarar variables 
+const WORD = "elocuencia";
+const LETTER_KEYS = {}; 
+let key = 0;
 
-// Diccionario para asignar 
-for (let letter = 0; letter < WORD.length; index++) {
-  console.log(WORD[letter]);
+
+// Asignar claves a las letras de la palabra
+for (let i = 0; i < WORD.length; i++) {
+  // Capturar letra en esa posición
+  const LETTER = WORD[i];
+  // Verificar si la letra ya existe en el diccionario
+  if (LETTER_KEYS[LETTER] === undefined) {
+    // No existe, agregar al diccionario y asignar una clave
+    LETTER_KEYS[LETTER] = key;
+    key++;
+  }
 }
+
+// Función para encriptar texto
+function encrypt(text) {
+  let result = "";
+  for (let char of text.toLowerCase()) {
+    if (LETTER_KEYS[char] !== undefined) {
+      result += LETTER_KEYS[char];
+    } else {
+      result += char;
+    }
+  }
+  return result;
+}
+
+// Capturar entradad del usuario
+const texto = document.getElementById("texto");
+// 
